1bd5c155d30baa1ff6757c5e3a63755484792355
sofichanapi22 <chanapisofia22@gmail.com>
2024-08-06 19:51:16 -0300

    tarea entregada

---
+++ inductancia.vb
@@ -0,0 +1,14 @@
+﻿Module module1
+    Sub main()
+        Dim frecuencia As Double
+        Dim inductancia As Double
+        Dim impedancia As Double
+
+        Console.Write("ingrese la frecuencia (Hz): ")
+        frecuencia = Console.ReadLine()
+        Console.Write("ingrese el valor de la inductancia (H) ")
+        inductancia = Console.ReaLine()
+        impedancia = 2 * XL pi + frecuencia + inductancia
+        Console.Write("la impedancia es:" & impedancia)
+
+        End Module
